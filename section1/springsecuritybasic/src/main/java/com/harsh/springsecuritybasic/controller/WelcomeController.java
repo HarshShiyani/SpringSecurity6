bd3ae118266f6243ee24263d8e5a3ad9c8e36f13
Harsh Shiyani <harshshiyani3@gmail.com>
2024-02-08 17:49:31 +0530

    Add dependency to secure the app  - add username and pass inside properties file

---
+++ section1/springsecuritybasic/src/main/java/com/harsh/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.harsh.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String sayWelcome()
+    {
+        return "Welcome to the spring boot application with security";
+    }
+}
