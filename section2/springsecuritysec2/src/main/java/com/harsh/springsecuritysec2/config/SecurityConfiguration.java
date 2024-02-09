cf65fda7026ded392c6c29d31e206b8aa83928e1
Harsh Shiyani <harshshiyani3@gmail.com>
2024-02-09 06:30:53 +0530

    Custom security configurations

---
+++ section2/springsecuritysec2/src/main/java/com/harsh/springsecuritysec2/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.harsh.springsecuritysec2.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(requests -> requests
+            .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans")
+            .authenticated()
+            .requestMatchers("/contact", "/notices").permitAll());
+
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+
+        return http.build();
+
+/*
+        Permit All the coming request
+        http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults());
+*/
+
+/*
+        Deny all the coming request
+        http.authorizeHttpRequests(request -> request.anyRequest().denyAll())
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults());
+*/
+    }
+}
