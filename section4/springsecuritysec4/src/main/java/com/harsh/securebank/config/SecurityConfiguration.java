0a062bc932bc220910981a383b6802abf8191f01
Harsh Shiyani <harshshiyani3@gmail.com>
2024-02-13 21:28:54 +0530

    Use BCryptPasswordEncoder for password

--- section4/springsecuritysec4/src/main/java/com/harsh/securebank/config/SecurityConfiguration.java
+++ section4/springsecuritysec4/src/main/java/com/harsh/securebank/config/SecurityConfiguration.java
@@ -8,11 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,41 +32,9 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-//    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
-    {
-/*
-        Approach 1: With default password encoder
-
-        UserDetails adminUser = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("admin")
-            .authorities("admin")
-            .build();
-
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-            .username("user1")
-            .password("user1")
-            .authorities("read")
-            .build();
-*/
-
-        UserDetails adminUser = User.withUsername("admin")
-            .password("admin")
-            .authorities("admin")
-            .build();
-
-        UserDetails user1 = User.withUsername("user1")
-            .password("user1")
-            .authorities("read")
-            .build();
-
-        return new InMemoryUserDetailsManager(adminUser, user1);
-    }
-
     @Bean
     public PasswordEncoder noOpPasswordEncoder()
     {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- section4/springsecuritysec4/src/main/java/com/harsh/securebank/controller/LoginController.java
+++ section4/springsecuritysec4/src/main/java/com/harsh/securebank/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.harsh.securebank.controller;
 
 import com.harsh.securebank.entity.Customer;
 import com.harsh.securebank.repository.CustomerRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public LoginController(CustomerRepository customerRepository)
+    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder)
     {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
     public String createCustomer(@RequestBody Customer customer)
     {
+        String hashPassword = passwordEncoder.encode(customer.getPwd());
+        customer.setPwd(hashPassword);
         customerRepository.save(customer);
         return "Customer saved successfully";
     }
