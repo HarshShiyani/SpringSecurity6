fa804c4f7b2c284a0f967e82f470ca92c14854c7
Harsh Shiyani <harshshiyani3@gmail.com>
2024-03-15 15:06:39 +0530

    section 12 initial commit

---
+++ section12/SpringSecOAuth2Github/src/main/java/com/harsh/SpringSecOAuth2Github/SpringSecOAuth2GithubApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.SpringSecOAuth2Github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOAuth2GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecOAuth2GithubApplication.class, args);
+	}
+
+}
---
+++ section12/SpringSecOAuth2Github/src/test/java/com/harsh/SpringSecOAuth2Github/SpringSecOAuth2GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.SpringSecOAuth2Github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecOAuth2GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
